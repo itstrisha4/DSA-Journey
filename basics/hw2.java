7402d86e2f556ec90495a9431e06bc048ea803f6
Trisha <shettytrisha636@gmail.com>
2026-08-25 12:59:40 +0530

    calculating if the number is positive or negative using ternary operator

---
+++ basics/hw2.java
@@ -0,0 +1,8 @@
+public class hw2 {
+    public static void main(String[] args) {
+        int num=5;
+        String type=(num>0)? "positive":"negative";
+        System.out.println(type);
+    }
+    
+}
