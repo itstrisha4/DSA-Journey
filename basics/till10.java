01e97c20e3bc3a67ce0ddd63c5d9a13ad458f3c9
Trisha <shettytrisha636@gmail.com>
2026-08-26 00:13:14 +0530

    display all the no except mutiple of 10

---
+++ basics/till10.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class till10 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+       
+      while(true){
+            int n=sc.nextInt();
+            if(n%10==0){
+                
+                break;
+               
+            }
+            System.out.println(n);
+        }
+    }
+}
