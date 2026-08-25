c15450df331c374f6a6a414303ff08960154f28e
Trisha <shettytrisha636@gmail.com>
2026-08-25 13:07:21 +0530

    problem 2 on ternary operator

---
+++ basics/hw3.java
@@ -0,0 +1,8 @@
+public class hw3 {
+    public static void main(String[] args) {
+        double temp=103.5;
+        String tem=(temp>100)?"fever":"no_your_normal";
+    System.out.println(tem);
+    }
+    
+}
