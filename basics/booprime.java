dcda785f5d02b86b2b3f96119fa91b0f8c0f4f59
Trisha <shettytrisha636@gmail.com>
2026-09-01 19:10:09 +0530

    prime using boolean and new function

---
+++ basics/booprime.java
@@ -0,0 +1,12 @@
+public class booprime{
+    public static boolean prime(int n){
+        for(int i=2;i<=n-1;i++){
+            if( n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String args[]){
+System.out.println(prime(5));    }
+}
