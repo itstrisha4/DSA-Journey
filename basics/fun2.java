fdcb917ba1958aa8af9f8b799352b93c011c3f40
Trisha <shettytrisha636@gmail.com>
2026-09-25 14:54:32 +0530

    boolean+even+function-2

---
+++ basics/fun2.java
@@ -0,0 +1,12 @@
+public class fun2 {
+    public static boolean isEven(int n){
+if(n%2==0){
+    return true;
+}else{
+    return false;
+}
+    }
+    public static void main(String args[]){
+System.out.println(isEven(5)
+);    }
+}
