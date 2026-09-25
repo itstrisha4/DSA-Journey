34e7f19f5fbfc75d024e9716a89d100004b7eaf6
Trisha <shettytrisha636@gmail.com>
2026-09-25 14:30:50 +0530

    decimal into binary

--- basics/decintobin.java
+++ basics/decintobin.java
@@ -1,12 +1,23 @@
 public class decintobin {
+   
     public static void  hi(int n){
+         int pow=0;
+    int dec=0;
         while(n != 0){
         int rem=n%2;
-        System.out.print(rem);
+       
+         dec=dec + rem *   (int)Math.pow(10,pow);
+        
+        pow++;
         n=n/2;
+       
         }
+         System.out.println(dec);
     }
+    
+     
+    
     public static void main(String[] args) {
-        hi(7);
+        hi(11);
     }
 }
